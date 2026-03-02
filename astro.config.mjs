e48ffa68c5e11dd018f227819fb3e55210a2eb49
Tashi 🏔️ <tashi@namche.ai>
2026-03-02 23:33:38 +0100

    Fix full build by removing unstable auto-embed path
    Co-authored-by: Jodok Batlogg <jodok@batlogg.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import embeds from 'astro-embed/integration';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://batlogg.com',
-  integrations: [embeds(), mdx(), sitemap()],
+  integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
